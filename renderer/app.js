feaea15b32c3f2017848ce446f8a7ad47b59acdf
xCONFLiCTiONx <info@xconflictionx.cc>
2026-09-24 18:28:39 -0500

    refactor: modify app.js (+1/-1)

--- renderer/app.js
+++ renderer/app.js
@@ -2835,7 +2835,7 @@ function switchConsoleTab(tab) {
         window.terminal.fitAddon.fit();
         window.terminal.term.focus();
       }, 50);
-    } else if (targetId === 'console-output') {
+    } else if (targetId === 'console-output' || targetId === 'developer-console-output') {
       // Force scroll to bottom when switching to console tab
       target.scrollTop = target.scrollHeight;
     }
